25a383a73c0af082f0b3ea5a500319b3fbb14767
dopadream <slyshidiscord@gmail.com>
2022-09-19 22:30:01 -0500

    a

--- src/main/java/net/sydokiddo/odyssey/mixin/block_tweaks/MixinLavaFluid.java
+++ src/main/java/net/sydokiddo/odyssey/mixin/block_tweaks/MixinLavaFluid.java
@@ -22,7 +22,6 @@ public class MixinLavaFluid {
                     target = "Lnet/minecraft/world/level/LevelAccessor;setBlock(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;I)Z"),
             index = 1)
     protected BlockState deepslate$flow(BlockPos pos, BlockState state, int flag) {
-        System.out.println(pos.toString());
         int y_level = pos.getY();
         // If Y value <= 0, 100% chance to generate Deepslate
         // Else if Y value <= 8, (9 - y) * 12.5% chance to generate Deepslate
